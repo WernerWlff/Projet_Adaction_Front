c36680891be4c8406a95afee46f714fbb004ae14
Werner Wolff <werner.wolff1109@gmail.com>
2025-10-09 15:23:12 +0200

    add : creation of the services need : methods to get data

--- Projet_Adaction_Front/src/app/services/city.ts
+++ Projet_Adaction_Front/src/app/services/city.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Coordinate } from './coordinate';
+
+export interface City{
+  id : number,
+  name : String,
+  coordinates_id : number,
+  coordinates : Coordinate
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class City {
-  
+  private ApiUrl = "./api/cities";
 }
+
