cfdda8d89e27a7920e4b45e87fdaae5f16b4843f
Werner Wolff <werner.wolff1109@gmail.com>
2025-10-09 15:23:12 +0200

    add : creation of the services need : methods to get data

--- Projet_Adaction_Front/src/app/services/collect.ts
+++ Projet_Adaction_Front/src/app/services/collect.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { City } from './city';
+import { Volunteer } from './volunteer';
 
+export interface Collect{
+  id : number,
+  date : Date,
+  city_id : number,
+  glass_nb : number,
+  butt_nb : number,
+  plastic_nb : number,
+  electronics_nb : number,
+  others_nb : number,
+  volunteer_id : number,
+  city : City;
+  volunteer : Volunteer;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class Collect {
-  
+  private ApiUrl = "./api/collects";
+
 }
--- Projet_Adaction_Front/src/app/services/coordinate.ts
+++ Projet_Adaction_Front/src/app/services/coordinate.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Coordinate{
+id : number,
+lat : number,
+lng : number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class Coordinate {
-  
+  private ApiUrl = "./api/coordinates";
 }
